ab6af6f4f985ebc7a2e93d34ff3ce27a7141ccc0
Nikita <chip_05@inbox.ru>
2021-08-04 16:54:37 +0300

    Project finished

--- script/burger-script-menu.dev.js
+++ script/burger-script-menu.dev.js
@@ -1,11 +1,6 @@
 "use strict";
 
 var menu_btn = document.querySelector('.burger-btn');
-window.addEventListener('load', function () {
-  if (document.querySelector('body').offsetWidth <= 680) {
-    menu_btn.classList.toggle('active-burger');
-  }
-});
 menu_btn.addEventListener('click', function () {
   document.querySelector('aside.aside').classList.toggle('active-aside');
 });
