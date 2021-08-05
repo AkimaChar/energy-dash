fcbafba99eeae43662955d239af35f99329c4aef
Nikita <chip_05@inbox.ru>
2021-08-05 16:13:16 +0300

    Additional page

---
+++ stacking_script.js
@@ -0,0 +1,7 @@
+// Change active menu button
+document.querySelectorAll(".nav_button").forEach((el) => {
+    el.classList.remove("active");
+  });
+  document.querySelector(".stacking").classList.add("active");
+  
+  
